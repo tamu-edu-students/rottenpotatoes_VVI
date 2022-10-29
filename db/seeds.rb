1042dae13f1c95fbff504fb34025b3276b783705
Vivek Vamsi Inala <vivekvamsi@Viveks-MacBook-Air.local>
2022-10-28 20:14:18 -0500

    completed part-4

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Jersey', :rating => '12A',
+    :release_date => '19-Apr-2019'}
 ]
 
 more_movies.each do |movie|
